266b258c3e2e675e80990ed3c27e8908be34c17b
Jamcintyre <137636739+Jamcintyre@users.noreply.github.com>
2023-06-24 11:46:25 -0400

    Revised Jdoc, cleaned formatting

--- McWBalance/src/main/java/com/mcwbalance/McWBalance.java
+++ McWBalance/src/main/java/com/mcwbalance/McWBalance.java
@@ -10,28 +10,26 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /**
- *
- * @author Alex
+ * McWBalance is intended to function as a deterministic daily time step mine 
+ * water balance planner and solver. The intention is to allow simplified 
+ * flowsheet development and operational logic entry that can be visually reviewed.
+ * The program is attempting to balance flexibility to allow any and all mine water
+ * balance problems to be solved, however, it will remain ridged in enforcing
+ * non-material stylistic rules (i.e. font size, chart styles, output format) to
+ * ensure consistency between users and projects.
+ * @author amcintyre
  */
 public class McWBalance {
     static BufferedImage mainIcon30;
-
     public static void main(String[] args) {
-        
          try {    
             mainIcon30 = ImageIO.read(new File("bin/Icon30.png"));
          }
          catch (IOException e){
             System.out.println("IconFile Not Found");
         }
-        
         System.out.println("McBalance Version " + ProjSetting.verInfo + " is loading");
-        
         MainWindow mainWin = new MainWindow();
-        mainWin.MainWindowFunct();
-        
+        mainWin.MainWindowFunct(); 
     }
-    
-    
-   
 }
