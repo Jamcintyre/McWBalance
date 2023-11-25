163c921eae4d747715f1beb6dd0ac252265ffd76
Jamcintyre <137636739+Jamcintyre@users.noreply.github.com>
2023-11-24 22:52:45 -0500

    Re-Factoring

--- McWBalance/src/main/java/com/mcwbalance/dacapacity/DACPlotWIndow.java
+++ McWBalance/src/main/java/com/mcwbalance/dacapacity/DACPlotWIndow.java
@@ -10,17 +10,17 @@ import javax.swing.JFrame;
  *
  * @author amcintyre
  */
-public class DataDACPlotWIndow extends JFrame{
+public class DACPlotWindow extends JFrame{
     
     private int windowSizeX = 1360;
     private int windowSizeY = 800+37;
 
-    public void plotWindow(DataDAC dAC){
+    public void plotWindow(DAC dAC){
          
         JFrame frame = new JFrame("DAC PLOT");
     
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        DataDACPlotGraphics plot = new DataDACPlotGraphics(dAC);
+        DACPlotGraphics plot = new DACPlotGraphics(dAC);
         frame.setSize(windowSizeX, windowSizeY);
         frame.add(plot);
         frame.setVisible(true);
